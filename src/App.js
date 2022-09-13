9c05a87e3b84351486cb0d54671f71f2f64466b2
Kevin Castro <castrokevin7@gmail.com>
2022-09-13 22:37:51 +0200

    Modify homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Visca TicoCat! (coming soon..)
+        Visca TicoCat! (coming soon.. for admins)
       </header>
     </div>
   );
