1e653d94bae5e47d2bff6a5fd8608401b492fd33
Kevin Castro <castrokevin7@gmail.com>
2022-09-20 21:45:20 +0200

    Putting associates in a view

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import AssociatesView from './components/AssociatesView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Visca TicoCat! (coming soon.. for admins)
-        <AssociatesView/>
-      </header>
+    <div>
+      <h1 id='header'>Aso TicoCat</h1>
+      <h2>Administración de Socios</h2>
+      <AssociatesView/>
     </div>
   );
 }
--- src/components/AssociatesView.tsx
+++ src/components/AssociatesView.tsx
@@ -1,25 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Associate } from '../models';
 import { DataStore } from 'aws-amplify';
 
-
-async function onQuery() {
-  const associates = await DataStore.query(Associate);
-  console.log(associates);
-}
-
 function AssociatesView() {
+  const [state, setState] = useState('');
+  const [associates, setAssociates] = useState<Associate[]>([]);
+
   useEffect(() => {
+    setState('loading');
+
     const subscription = DataStore.observe(Associate).subscribe((msg) => {
       console.log(msg.model, msg.opType, msg.element);
+    });
+
+    DataStore.query(Associate)
+    .then((response) => {
+      console.log(response);
+      setAssociates(response);
+      setState('success');
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      setState('error');
     });
 
     return () => subscription.unsubscribe();
   }, []);
   
+  if (state === 'error')
+    return (
+      <h1>
+        Hubo un error...
+      </h1>
+    );
   return (
     <div>
-       <input type="button" value="Log members" onClick={onQuery} />
+      <div>
+        {state === 'loading' ? (
+          <p>Cargando...</p>
+        ) : (
+          <div>
+            <h3>Socios:</h3>
+            {associates.map((a: Associate, i) => <p key={i}>{a.name}</p>)}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
