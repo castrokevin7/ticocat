1b0ce8cfcc0f92bc33c07eaa0455e3a19ed2155a
Kevin Castro <castrokevin7@gmail.com>
2022-09-23 23:23:44 +0200

    Display associate information

--- src/components/AssociatesView.tsx
+++ src/components/AssociatesView.tsx
@@ -3,16 +3,34 @@ import { Associate } from '../models';
 import { DataStore } from 'aws-amplify';
 import DeleteIcon from '@mui/icons-material/Delete';
 import './AssociatesView.css';
+import { Box, Modal } from '@mui/material';
+import PageviewIcon from '@mui/icons-material/Pageview';
+import CloseIcon from '@mui/icons-material/Close';
 
 type QueryExpressionsMap = { 
   [searchKey: string]: (searchValue: string) => Promise<Associate[]>; 
 }
+
+const modalStyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  color: 'black',
+};
 
 function AssociatesView() {
   const [state, setState] = useState('');
   const [searchBy, setSearchBy] = useState('name');
   const [searchValue, setSearchValue] = useState('');
   const [associates, setAssociates] = useState<Associate[]>([]);
+  const [associate, setAssociate] = useState<Associate>();
+  const [openViewAssociate, setOpenViewAssociate] = React.useState(false);
 
   const QUERY_EXPRESSIONS: QueryExpressionsMap = {
     'name': (searchValue: string) => DataStore.query(Associate, a => a.name("contains", searchValue)),
@@ -99,6 +117,14 @@ function AssociatesView() {
                 return (
                   <div key={i} className='associate'>
                     <span 
+                      className='view-associate'
+                      onClick={() => {
+                        setAssociate(a);
+                        setOpenViewAssociate(true);
+                      }}>
+                      <PageviewIcon />
+                    </span>
+                    <span 
                       className='delete-associate'
                       onClick={async () => {
                         if (window.confirm('¿Confirma la eliminación del Socio (irreversible)?')) {
@@ -116,8 +142,37 @@ function AssociatesView() {
           </div>
         )}
       </div>
+      { associate ? (
+        <Modal
+          open={openViewAssociate}
+          onClose={() => setOpenViewAssociate(false)}
+        >
+          <Box sx={modalStyle}>
+            <CloseIcon id='close-view-associate' onClick={() => setOpenViewAssociate(false)} />
+            <p>Nombre: {associate.name}</p>
+            {associate.board_position ?
+             <p>Posición: {capitalizeFirst(associate.board_position)}</p>
+             : null}
+            <p>Fecha de inscripción: {associate.inscription_date}</p>
+            <p>Fecha de nacimiento: {associate.birthday}</p>
+            <p>Dirección: {associate.address}</p>
+            <p>Correo: {associate.email}</p>
+            <p>Teléfono: {associate.phone}</p>
+            <p>Nacionalidad: {associate.nationality}</p>
+            <p>Tipo de Identificación: {associate.identification_type}</p>
+            <p>Identificación: {associate.identification}</p>
+          </Box>
+        </Modal>
+      ) : null }
+
     </div>
   );
 }
 
 export default AssociatesView;
+
+function capitalizeFirst(arg: string): string {
+  const lower = arg.toLowerCase();
+  return arg[0] + lower.slice(1);
+}
+
