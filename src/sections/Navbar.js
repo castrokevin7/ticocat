d96fd205d8b4c7240d00e4e6797472a367aa789b
Kevin Castro <castrokevin7@gmail.com>
2022-12-08 21:03:28 +0100

    Add button to change language page

--- src/sections/Navbar.js
+++ src/sections/Navbar.js
@@ -3,6 +3,24 @@ import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 
 import routes from "routes";
 
+function getAction() {
+  let domain = window.location.hostname;
+  if (domain === "www.asoticocat.com") {
+      return {
+        type: "external",
+        route: "https://cat.asoticocat.com",
+        label: "CAT",
+        color: "info",
+      }
+  }
+  return {
+    type: "external",
+    route: "https://www.asoticocat.com",
+    label: "ESP",
+    color: "info",
+  }
+}
+
 function Navbar() {
   return (
     <DefaultNavbar
@@ -10,6 +28,7 @@ function Navbar() {
       center
       sticky
       brand="asoticocat"
+      action={getAction()}
     />
   );
 }
