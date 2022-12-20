4de7d77e6f838660de890d736ee4bde3d83bebe7
Kevin Castro <castrokevin7@gmail.com>
2022-12-20 17:23:56 +0100

    Navbar for Events and Event page

--- src/sections/events/EventPage.js
+++ src/sections/events/EventPage.js
@@ -9,6 +9,8 @@ import MKBox from "components/MKBox";
 import MKBadge from "components/MKBadge";
 import MKTypography from "components/MKTypography";
 
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
 // Images
 import bg1 from "assets/images/bg.jpg";
 import bg2 from "assets/images/examples/content-1.jpg";
@@ -21,109 +23,122 @@ function EventPage() {
   const { id } = useParams();
 
   return (
-    <MKBox component="section" py={6}>
-      <Container>
-        <Grid
-          container
-          item
-          xs={8}
-          flexDirection="column"
-          alignItems="center"
-          mx="auto"
-          textAlign="center"
-          mb={6}
-        >
-          <MKBadge
-            badgeContent="co-working"
-            variant="contained"
-            color="info"
-            container
-            sx={{ mb: 1 }}
-          />
-          <MKTypography variant="h2" mb={1}>
-            Event {id}
-          </MKTypography>
-          <MKTypography variant="body2" color="text">
-            If you can&apos;t decide, the answer is no. If two equally difficult paths, choose the
-            one more painful in the short term (pain avoidance is creating an illusion of equality).
-          </MKTypography>
-        </Grid>
-        <Grid container spacing={3} minHeight="40vh">
-          <Grid item xs={5} sm={4}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg1})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-          <Grid item xs={7} sm={3}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg2})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={5}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg3})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-          <Grid item xs={7} sm={3}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg4})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-          <Grid item xs={5}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg5})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <MKBox
-              width="100%"
-              height="100%"
-              borderRadius="lg"
-              shadow="md"
-              sx={{
-                backgroundImage: `url(${bg6})`,
-                backgroundSize: "cover",
-              }}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-    </MKBox>
+    <>
+        <DefaultNavbar
+            routes={[]}
+            center
+            sticky
+            brand="asoticocat"
+            action={{
+                route: "/eventos",
+                label: "<-",
+                color: "info",
+            }}
+        />
+        <MKBox component="section" py={6}>
+            <Container>
+                <Grid
+                container
+                item
+                xs={8}
+                flexDirection="column"
+                alignItems="center"
+                mx="auto"
+                textAlign="center"
+                mb={6}
+                >
+                <MKBadge
+                    badgeContent="co-working"
+                    variant="contained"
+                    color="info"
+                    container
+                    sx={{ mb: 1 }}
+                />
+                <MKTypography variant="h2" mb={1}>
+                    Event {id}
+                </MKTypography>
+                <MKTypography variant="body2" color="text">
+                    If you can&apos;t decide, the answer is no. If two equally difficult paths, choose the
+                    one more painful in the short term (pain avoidance is creating an illusion of equality).
+                </MKTypography>
+                </Grid>
+                <Grid container spacing={3} minHeight="40vh">
+                <Grid item xs={5} sm={4}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg1})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                <Grid item xs={7} sm={3}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg2})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={5}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg3})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                <Grid item xs={7} sm={3}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg4})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                <Grid item xs={5}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg5})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <MKBox
+                    width="100%"
+                    height="100%"
+                    borderRadius="lg"
+                    shadow="md"
+                    sx={{
+                        backgroundImage: `url(${bg6})`,
+                        backgroundSize: "cover",
+                    }}
+                    />
+                </Grid>
+                </Grid>
+            </Container>
+        </MKBox>
+    </>
   );
 }
 
--- src/sections/events/EventsPage.js
+++ src/sections/events/EventsPage.js
@@ -17,53 +17,115 @@ import post1 from "assets/images/examples/blog1.jpg";
 import post2 from "assets/images/examples/blog2.jpg";
 import post3 from "assets/images/examples/blog3.jpg";
 
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// @mui material components
+import Stack from "@mui/material/Stack";
+
+import MKButton from "components/MKButton";
+
+// Images
+import bgImage from "assets/images/bg-coworking.jpeg";
+
 function EventsPage() {
-  return (
-    <MKBox component="section" py={6}>
-      <Container>
-        <Grid container item xs={12} lg={6} flexDirection="column">
-          <MKTypography variant="h3" mt={3} mb={1}>
-            Build something great
-          </MKTypography>
-          <MKTypography variant="body2" color="text" mb={2}>
-            We&apos;re constantly trying to express ourselves and actualize our dreams. If you have
-            the opportunity to play this game of life you need to appreciate every moment.
-          </MKTypography>
-        </Grid>
-        <Grid container spacing={3} mt={3}>
-          <Grid item xs={12} lg={4}>
-            <Link to="/sections/page-sections/blog-posts">
-              <SimpleBackgroundCard
-                image={post1}
-                title="Search and Discovery"
-                description="Website visitors today demand a frictionless user expericence — especially when
-                    using search. Because of the hight standards we tend to offer."
-              />
-            </Link>
+  return (    
+    <>
+      <DefaultNavbar
+          routes={[]}
+          center
+          sticky
+          brand="asoticocat"
+          action={{
+            route: "/",
+            label: "<-",
+            color: "info",
+          }}
+      />
+      <MKBox component="header" position="relative">
+        <MKBox
+          display="flex"
+          alignItems="center"
+          minHeight="100vh"
+          sx={{
+            backgroundImage: ({ palette: { gradients }, functions: { linearGradient, rgba } }) => `${linearGradient(rgba(gradients.dark.main, 0.5), rgba(gradients.dark.state, 0.5))}, url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Container>
+            <Grid container item xs={12} md={7} lg={6} flexDirection="column" justifyContent="center">
+              <MKTypography
+                variant="h1"
+                color="white"
+                mb={3}
+                sx={({ breakpoints, typography: { size } }) => ({
+                  [breakpoints.down("md")]: {
+                    fontSize: size["3xl"],
+                  },
+                })}
+              >
+                Material Kit
+              </MKTypography>
+              <MKTypography variant="body1" color="white" opacity={0.8} pr={6} mr={6}>
+                The time is now for it be okay to be great. People in this world shun people for being
+                nice.
+              </MKTypography>
+              <Stack direction="row" spacing={1} mt={3}>
+                <MKButton color="white">Get Started</MKButton>
+                <MKButton variant="text" color="white">
+                  Read more
+                </MKButton>
+              </Stack>
+            </Grid>
+          </Container>
+        </MKBox>
+      </MKBox>
+      <MKBox component="section" py={6}>
+        <Container>
+          <Grid container item xs={12} lg={6} flexDirection="column">
+            <MKTypography variant="h3" mt={3} mb={1}>
+              Build something great
+            </MKTypography>
+            <MKTypography variant="body2" color="text" mb={2}>
+              We&apos;re constantly trying to express ourselves and actualize our dreams. If you have
+              the opportunity to play this game of life you need to appreciate every moment.
+            </MKTypography>
           </Grid>
-          <Grid item xs={12} lg={4}>
-            <Link to="/sections/page-sections/blog-posts">
-              <SimpleBackgroundCard
-                image={post2}
-                title="Last visits in US"
-                description="Wealth creation is an evolutionarily recent positive-sum game. Status is an old
-                    zero-sum game. Those attacking wealth creation are often just seeking status."
-              />
-            </Link>
+          <Grid container spacing={3} mt={3}>
+            <Grid item xs={12} lg={4}>
+              <Link to="/sections/page-sections/blog-posts">
+                <SimpleBackgroundCard
+                  image={post1}
+                  title="Search and Discovery"
+                  description="Website visitors today demand a frictionless user expericence — especially when
+                      using search. Because of the hight standards we tend to offer."
+                />
+              </Link>
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <Link to="/sections/page-sections/blog-posts">
+                <SimpleBackgroundCard
+                  image={post2}
+                  title="Last visits in US"
+                  description="Wealth creation is an evolutionarily recent positive-sum game. Status is an old
+                      zero-sum game. Those attacking wealth creation are often just seeking status."
+                />
+              </Link>
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <Link to="/sections/page-sections/blog-posts">
+                <SimpleBackgroundCard
+                  image={post3}
+                  title="Grow in a beautiful area"
+                  description="Free people make free choices. Free choices mean you get unequal outcomes. You
+                      can have freedom, or you can have equal outcomes. You can't have both."
+                />
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12} lg={4}>
-            <Link to="/sections/page-sections/blog-posts">
-              <SimpleBackgroundCard
-                image={post3}
-                title="Grow in a beautiful area"
-                description="Free people make free choices. Free choices mean you get unequal outcomes. You
-                    can have freedom, or you can have equal outcomes. You can't have both."
-              />
-            </Link>
-          </Grid>
-        </Grid>
-      </Container>
-    </MKBox>
+        </Container>
+      </MKBox>
+    </>
   );
 }
 
