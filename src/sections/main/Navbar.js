60c3f200b98379acb7d166afb94127d2619e9ced
Kevin Castro <castrokevin7@gmail.com>
2022-12-25 17:29:15 +0100

    Fix translate path

--- src/sections/main/Navbar.js
+++ src/sections/main/Navbar.js
@@ -4,39 +4,40 @@ import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 import routes from "routes";
 
 export function getTranslateAction() {
-  let domain = window.location.hostname;
-  if (domain === "www.asoticocat.com") {
-      return {
-        route: "https://cat.asoticocat.com",
-        label: "CAT",
+    let domain = window.location.hostname;
+    let path = window.location.pathname;
+    if (domain === "www.asoticocat.com") {
+        return {
+            route: "https://cat.asoticocat.com" + path,
+            label: "CAT",
+            color: "info",
+            icon: "translate_rounded",
+            variant: "outlined",
+            size: "small",
+            minimal: false
+        }
+    }
+    return {
+        route: "https://www.asoticocat.com" + path,
+        label: "ESP",
         color: "info",
         icon: "translate_rounded",
         variant: "outlined",
         size: "small",
         minimal: false
-      }
-  }
-  return {
-    route: "https://www.asoticocat.com",
-    label: "ESP",
-    color: "info",
-    icon: "translate_rounded",
-    variant: "outlined",
-    size: "small",
-    minimal: false
-  }
+    }
 }
 
 function Navbar() {
-  return (
-    <DefaultNavbar
-      routes={routes}
-      center
-      sticky
-      brand="asoticocat"
-      action={getTranslateAction()}
-    />
-  );
+    return (
+        <DefaultNavbar
+            routes={routes}
+            center
+            sticky
+            brand="asoticocat"
+            action={getTranslateAction()}
+        />
+    );
 }
 
 export default Navbar;
