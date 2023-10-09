ea128934bc31bc9f6d5057a508644672ba5e847e
Kevin Castro <castrokevin7@gmail.com>
2023-10-09 18:38:08 +0200

    Fix card Margin in FAQ view

--- src/sections/faqs/FAQView.js
+++ src/sections/faqs/FAQView.js
@@ -12,6 +12,7 @@ import { getTranslateAction } from "sections/main/Navbar";
 import bgImage from "assets/images/faqs.jpeg";
 import Card from "@mui/material/Card";
 import { Spinner } from "sections/common/Spinner";
+import { auto } from "@popperjs/core";
 
 
 
@@ -49,6 +50,7 @@ function FAQView() {
                     backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
                     backdropFilter: "saturate(200%) blur(30px)",
                     boxShadow: ({ boxShadows: { xxl } }) => xxl,
+                    overflowY: auto
                 }}
             >
                 <MKTypography
@@ -132,7 +134,9 @@ function FAQView() {
                         backgroundPosition: "center",
                     }}
                 >
-                    <Container>
+                    <Container
+                        sx={{ marginTop: '150px', marginBottom: '100px' }}
+                    >
                         {getFAQContent(faq)}
                     </Container>
                 </MKBox>
