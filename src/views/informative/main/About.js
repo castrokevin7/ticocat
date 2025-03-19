0b7648b56185becfa0ab7f6e098f00edd2b6b967
Kevin Castro <castrokevin7@gmail.com>
2025-03-19 16:03:54 +0100

    Include Vermutico Podcast link

--- src/views/informative/main/About.js
+++ src/views/informative/main/About.js
@@ -39,6 +39,15 @@ function About() {
             <MKTypography variant="h3" my={1} mb={2}>
               {Translator.instance.translate("about_context_title")}
             </MKTypography>
+            <iframe
+              style={{ borderRadius: "12px", marginBottom: "20px" }}
+              src="https://open.spotify.com/embed/episode/6FzLsELhfLPm4CGWDzoyjf?utm_source=generator&theme=0"
+              width="100%"
+              height="152"
+              frameBorder="0"
+              allowFullScreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy" />
             <MKTypography variant="body1" color="text" mb={2}>
               {Translator.instance.translate("about_context_description")}
             </MKTypography>
