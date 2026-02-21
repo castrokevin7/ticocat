3700211cbb6ee50b9b38383d3829d9dd88faab28
jairo <jsanchez@secro.io>
2026-02-21 11:19:03 +0100

    prueba

--- src/views/informative/main/About.js
+++ src/views/informative/main/About.js
@@ -38,6 +38,7 @@ function About() {
           <Grid container mb={2}>
             <MKTypography variant="h3" my={1} mb={2}>
               {Translator.instance.translate("about_context_title")}
+              No se esto es una prueba lo que sea
             </MKTypography>
             <iframe
               title="Spotify Player"
